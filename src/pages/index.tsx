d14478c657943ce64791dd01de89c1c07d517fe9
Shun Nakano <nakano_syun@waku-2.com>
2021-01-29 15:08:11 +0900

    feat: pages/indexの作成

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components/Layout';
+import { Calculator } from '../components/Calculator';
+
+const index: React.VFC = () => (
+  <Layout>
+    <Calculator />
+  </Layout>
+);
+
+export default index;
